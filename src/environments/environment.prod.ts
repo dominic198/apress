cb1da948e4dde13e838e7fca236c380e223fa6b8
Dominic <dominicsavio8@gmail.com>
2021-07-04 10:08:22 +0530

    changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+  apiKey: 'AIzaSyAAp2FDfXVZ4gCACGd_Irs2tpO2InrVcU8',
+  authDomain: 'awesome-project-a1a25.firebaseapp.com',
+  databaseURL: '<your-database-URL>',
+  projectId: 'awesome-project-a1a25',
+  storageBucket: 'awesome-project-a1a25.appspot.com',
+  messagingSenderId: '437844802084'
+  }
 };
